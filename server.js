75180c15fdc72e9a3d8abf3430efc1f50af555e2
Vidya Sagar <46023405+Vidyasagar-DevOps@users.noreply.github.com>
2024-08-31 17:04:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hearty Congratulations on Pipeline');
+  res.send('Hearty Congratulations on first Pipeline');
 });
 
 app.listen(PORT, HOST);
